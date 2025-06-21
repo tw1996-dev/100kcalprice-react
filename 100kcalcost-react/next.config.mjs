d15b0972dafdcd9e9453a40e9559b4e93ec531c0
tw1996-dev <19tomaszwojcik96@gmail.com>
2025-06-22 00:00:14 +0200

    gh-pages fix

--- 100kcalcost-react/next.config.mjs
+++ 100kcalcost-react/next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
 export const nextConfig = {
   output: 'export',
+  basePath: '/100kcalprice-react',
+  assetPrefix: '/100kcalprice-react',
 };
 
 export default nextConfig;
